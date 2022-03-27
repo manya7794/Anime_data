29cbd897b7bf9b4396d38bb407c62fc3da9b1ed1
Trystan Despierres <despierrestry@yahoo.fr>
2022-03-28 01:28:16 +0200

    Définition des fonctions

--- extractList.py
+++ extractList.py
@@ -2,57 +2,44 @@ import pandas as pd
 import numpy as np
 import xml.etree.ElementTree as et
 
-# Récupération du nom du fichier
-tree = et.parse("animelist.xml")
+def recupFichier(nom, id, score, etat):
+    # Récupération du nom du fichier
+    tree = et.parse("animelist.xml")
 
-# Récupération de la racine
-root = tree.getroot()
+    # Récupération de la racine
+    root = tree.getroot()
 
-#Identifiant de la série
-#print(root[1][0].text)
+    #Récupération de tous les noms dans une liste
+    for titre in root.iter("series_title"):
+        # print(identifiant.text)
+        nom.append(titre.text)
 
-#Titre de la série
-#print(root[1][1].text)
+    #Récupération de tous les identifiants dans une liste
+    for identifiant in root.iter("series_animedb_id"):
+        # print(identifiant.text)
+        id.append(identifiant.text)
 
-#Score donné à la série
-#print(root[1][9].text)
+    #Récupération de toutess les notes dans une liste
+    for note in root.iter("my_score"):
+        score.append(note.text)
 
-#Etat de visionnage de la série
-#print(root[1][12].text)
-
-nom = []
-id = []
-score = []
-etat = []
-
-#Récupération de tous les noms dans une liste
-for titre in root.iter("series_title"):
-    # print(identifiant.text)
-    nom.append(titre.text)
-
-#Récupération de tous les identifiants dans une liste
-for identifiant in root.iter("series_animedb_id"):
-    # print(identifiant.text)
-    id.append(identifiant.text)
-
-#Récupération de toutess les notes dans une liste
-for note in root.iter("my_score"):
-    score.append(note.text)
-
-#Récupération de tous les statuts de visionnage dans une liste
-for statut in root.iter("my_status"):
-    etat.append(statut.text)
+    #Récupération de tous les statuts de visionnage dans une liste
+    for statut in root.iter("my_status"):
+        etat.append(statut.text)
+    
+    return(nom, id, score, etat)
 
 
-#Création d'une dataframe
+def createDataFrame(nom, id, score, etat):
+    #Création d'une dataframe
+    anime_df = pd.DataFrame(
+        list(zip(
+            nom, id, score, etat
+        )),
+        columns=["Nom","Identifiant", "Note", "Statut"]
+    )
 
-anime_df = pd.DataFrame(
-    list(zip(
-        nom,id, score, etat
-    )),
-    columns=["Nom","Identifiant", "Note", "Statut"]
-)
+def saveCSV(dataframe):
+    #Sauvegarde de la dataframe au format CSV
+    dataframe.to_csv("animelist.csv")
 
-#Sauvegarde de la dataframe au format CSV
-
-anime_df.to_csv("animelist.csv")
